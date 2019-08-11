6e0c0c7cd10f687856cd51d1c909122160a57651
Aaron Riekenberg <aaron.riekenberg@gmail.com>
2019-08-11 09:40:14 -0500

    Add code

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC = cc
+CFLAGS = -g -Wall
+LDFLAGS =
+
+SRC = simplerotate.c
+OBJS = $(SRC:.c=.o)
+
+all: simplerotate
+
+clean:
+	rm -fr simplerotate *.o
+
+simplerotate: $(OBJS)
+	$(CC) $(LDFLAGS) $(OBJS) -o $@
+
+depend:
+	$(CC) $(CFLAGS) -MM $(SRC) > .makeinclude
+
+include .makeinclude
---
+++ simplerotate.c
@@ -0,0 +1,146 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define DEBUG_ENABLED 1
+#define LOCK_FILE_NAME "lock"
+#define OUTPUT_FILE_NAME "output"
+#define MAX_OUTPUT_FILE_SIZE_BYTES 100
+#define MAX_FILES 10
+
+#define DEBUG_PRINTF(fmt, ...) do { if (DEBUG_ENABLED) printf(fmt, ##__VA_ARGS__); } while (0)
+
+static void acquireLock() {
+  int fd;
+  int retVal;
+
+  DEBUG_PRINTF("acquireLock\n");
+
+  fd = open(LOCK_FILE_NAME, O_CREAT | O_WRONLY, 0644);
+  if (fd < 0) {
+    DEBUG_PRINTF("error opening lock errno = %s\n", strerror(errno));
+    exit(1);
+  }
+
+  retVal = flock(fd, LOCK_EX);
+  if (retVal < 0) {
+    DEBUG_PRINTF("error on flock errno = %s\n", strerror(errno));
+    exit(1);
+  }
+
+  DEBUG_PRINTF("acquired LOCK_EX lock on fd %d\n", fd);
+}
+
+static off_t getOutputFileBytes() {
+  off_t bytes = 0;
+  struct stat s;
+
+  if (stat(OUTPUT_FILE_NAME, &s) == 0) {
+    bytes = s.st_size;
+  }
+
+  return bytes;
+}
+
+static void rotateFiles() {
+  char fileName1[20];
+  char fileName2[20];
+  int i;
+
+  sprintf(fileName1, OUTPUT_FILE_NAME ".%d", MAX_FILES - 1);
+  DEBUG_PRINTF("unlink %s\n", fileName1);
+  unlink(fileName1);
+
+  for (i = MAX_FILES - 1; i >= 2; --i) {
+    sprintf(fileName1, OUTPUT_FILE_NAME ".%d", i - 1);
+    sprintf(fileName2, OUTPUT_FILE_NAME ".%d", i);
+    DEBUG_PRINTF("rename %s %s\n", fileName1, fileName2);
+    rename(fileName1, fileName2);
+  }
+
+  sprintf(fileName1, OUTPUT_FILE_NAME ".%d", 1);
+  DEBUG_PRINTF("rename %s %s\n", OUTPUT_FILE_NAME, fileName1);
+  rename(OUTPUT_FILE_NAME, fileName1);
+}
+
+int main(int argc, char** argv) {
+  char buffer[8192];
+  int outputFD;
+  ssize_t readRetVal;
+  ssize_t writeRetVal;
+  size_t bytesWritten;
+  int intRetVal;
+  off_t outputFileSize;
+
+  if (argc > 1) {
+    DEBUG_PRINTF("chdir %s\n", argv[1]);
+    intRetVal = chdir(argv[1]);
+    if (intRetVal < 0) {
+      DEBUG_PRINTF("chdir failed errno = %s\n", strerror(errno));
+      exit(1);
+    }
+  }
+
+  acquireLock();
+
+  outputFileSize = getOutputFileBytes();
+  DEBUG_PRINTF("initial outputFileSize = %zu\n", outputFileSize);
+
+  outputFD = open(OUTPUT_FILE_NAME, O_APPEND | O_CREAT | O_WRONLY, 0644);
+  if (outputFD < 0) {
+    DEBUG_PRINTF("error opening output errno = %s\n", strerror(errno));
+    exit(1);
+  }
+
+  while (true) {
+    readRetVal = read(STDIN_FILENO, buffer, sizeof(buffer));
+    if (readRetVal == 0) {
+      DEBUG_PRINTF("read returned 0\n");
+      exit(1);
+    } else if (readRetVal < 0) {
+      DEBUG_PRINTF("read returned < 0 errno = %s\n", strerror(errno));
+      exit(1);
+    }
+
+    DEBUG_PRINTF("read returned %zd\n", readRetVal);
+    do {
+      bytesWritten = 0;
+      writeRetVal = write(outputFD, buffer + bytesWritten, readRetVal);
+      if (writeRetVal < 0) {
+        DEBUG_PRINTF("write returned < 0 errno = %s\n", strerror(errno));
+        exit(1);
+      }
+
+      DEBUG_PRINTF("write returned %zd\n", writeRetVal);
+      readRetVal -= writeRetVal; 
+      bytesWritten += writeRetVal;
+      outputFileSize += writeRetVal;
+
+      if (outputFileSize >= MAX_OUTPUT_FILE_SIZE_BYTES) {
+        intRetVal = close(outputFD);
+        if (intRetVal < 0) {
+          DEBUG_PRINTF("close returned < 0 errno = %s\n", strerror(errno));
+          exit(1);
+	}
+
+        rotateFiles();       
+        outputFileSize = 0;
+
+        outputFD = open(OUTPUT_FILE_NAME, O_APPEND | O_CREAT | O_WRONLY, 0644);
+        if (outputFD < 0) {
+          DEBUG_PRINTF("error opening output errno = %s\n", strerror(errno));
+          exit(1);
+        }
+      }
+    } while (readRetVal > 0);
+  }
+
+  return 0;
+}
